1e44e8af00862c2329d3e802a9af848b98df7087
Bingen Galartza <bingen.galartza@magnet.coop>
2019-05-13 09:56:54 +0200

    [#4] Oinarrizko garapen ingurunea

---
+++ main-app/src/test/java/landakoop/landabus/mainapp/MainAppApplicationTests.java
@@ -0,0 +1,16 @@
+package landakoop.landabus.mainapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
