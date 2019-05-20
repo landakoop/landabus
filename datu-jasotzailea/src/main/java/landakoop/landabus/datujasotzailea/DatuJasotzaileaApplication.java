69efecab1e63d6bb4c909a5e426ad1d599eda55b
Bingen Galartza <bingen.galartza@alumni.mondragon.edu>
2019-05-20 08:49:09 +0200

    [#5] Datu jasotzailearen hasierako egitura sortu

---
+++ datu-jasotzailea/src/main/java/landakoop/landabus/datujasotzailea/DatuJasotzaileaApplication.java
@@ -0,0 +1,13 @@
+package landakoop.landabus.datujasotzailea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatuJasotzaileaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatuJasotzaileaApplication.class, args);
+	}
+
+}
