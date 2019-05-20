95929236aed8c1794ae8a9de7812ced7b8c87423
KepaUrzelai <kepa.urzelaiv@alumni.mondragon.edu>
2019-05-20 18:27:27 +0200

    [#2] SendMessage metodoa erabiliko dugu galdetegia bistaratzeko, SendPoll ezin baita txat pribatuetara bidali.

--- LandabusBot/src/main/java/landakoop/landabusBot/LandabusBot.java
+++ LandabusBot/src/main/java/landakoop/landabusBot/LandabusBot.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,19 +12,22 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
-//import org.telegram.telegrambots.meta.api.methods.send.;
+
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @Component
 public class LandabusBot extends TelegramLongPollingBot{
 	private static final Logger logger = LoggerFactory.getLogger(LandabusBot.class);
-	//static final String fUsername = "LandabusBot";
-	//static final String fToken = "839842086:AAGyahyGKbaWyX3tR4KDuQqveyJJ6gPjFUM";
-	//static final String[] arrayGeltokiak = {"AMAIUR","BERA","ZIGA","DONEZTEBE","ELIZONDO","ZUGARRAMURDI","GARTZAIN"};
+	static final String[] hasiera = {"KONTSULTATU"};
+	static final String[] arrayGeltokiak = {"AMAIUR","BERA","ZIGA","DONEZTEBE","ELIZONDO","ZUGARRAMURDI","GARTZAIN"};
+	static final String aurkezpena = "Ordutegien kontsulta edo eskakizun bat egiteko sakatu: KONTSULTATU ";
+	static final String irteeraGaldera = "Non hartu nahi duzu autobusa?";
+	static final String helmugaGaldera = "Nora heldu nahi duzu autobusean?";
+	
+	int stage;
 	
 	@Value("${bot.token}")
 	private String token;
@@ -47,61 +49,94 @@ public class LandabusBot extends TelegramLongPollingBot{
 	public void start() {
 		logger.info("username: {}, token: {}", username, token);
 	}
-	
-	@PreDestroy
-	public void kill() {
-			try {
-				this.finalize();
-			} catch (Throwable e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
 
 	@Override
 	public void onUpdateReceived(Update update) {
 		if (update.hasMessage()) {
 			Message message = update.getMessage();
-			SendMessage response = new SendMessage();
-			Long chatId = message.getChatId();
-			response.setChatId(chatId);
-			String text = message.getText();
-			response.setText(text);
-			try {
-				execute(response);
-				logger.info("Mezua \"{}\" , nori: {}", text, chatId);
-			} catch (TelegramApiException e) {
-				logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+			
+			if(message.getText().equals("/start")) stage = 0;
+			else if(message.getText().equals("KONTSULTATU")) stage = 1;
+			
+			switch(stage) {
+			case 0: try {
+					execute(mezuaBidali(message,aurkezpena));
+					logger.info("Hasiera orria");
+				} catch (TelegramApiException e2) {
+					logger.error("Errorea hasierako orria bistaratzean");
+					e2.printStackTrace();
+				}
+				break;
+			case 1: try {
+					execute(mezuaBidali(message,irteeraGaldera));
+					logger.info("Kontsultako lehenengo galdetegia bidalita \"{}\" erabiltzaileari", message.getChatId());
+					stage = 2;
+				} catch (TelegramApiException e1) {
+					logger.error("Failed to send message to {} due to error: {}", message.getChatId(), e1.getMessage());
+				}
+				break;
+			case 2: System.out.println(message.getText());
+				try {
+					execute(mezuaBidali(message,helmugaGaldera));
+					logger.info("Kontsultako lehenengo galdetegia bidalita \"{}\" erabiltzaileari", message.getChatId());
+					stage = 3;
+				} catch (TelegramApiException e1) {
+					logger.error("Failed to send message to {} due to error: {}", message.getChatId(), e1.getMessage());
+				}
+				break;
+			default: 
+				SendMessage response = new SendMessage();
+				
+				Long chatId = message.getChatId();
+				response.setChatId(chatId);
+				
+				String text = message.getText();
+				response.setText(text);
+				
+				try {
+					execute(response);
+					logger.info("Mezua \"{}\" , nori: {}", text, chatId);
+				} catch (TelegramApiException e) {
+					logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+				}
+				break;
 			}
 		}
 	}
 	
-	
-	@SuppressWarnings("unused")
-	private static ReplyKeyboardMarkup stringListToMarkup(List<String> list) {
-		ReplyKeyboardMarkup replyMarkup = new ReplyKeyboardMarkup();
-		
-		return replyMarkup;
+	private SendMessage mezuaBidali(Message message, String str) {
+		SendMessage msg = new SendMessage();
+		msg.setChatId(message.getChatId());
+		msg.setText(str);
+		msg.setReplyMarkup(kontsultaGaldetegia(stage));
+		return msg;
 	}
-
-	@SuppressWarnings("unused")
-	private static ReplyKeyboardMarkup getMainMenuKeyboard() {
+	
+	
+	private ReplyKeyboardMarkup kontsultaGaldetegia(int aukeraLista) {
 		ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
-        replyKeyboardMarkup.setOneTimeKeyboard(false);
-        
+        replyKeyboardMarkup.setOneTimeKeyboard(false);		
         List<KeyboardRow> keyboard = new ArrayList<>();
-        KeyboardRow keyboardRow = new KeyboardRow();
-        keyboardRow.add(getKontsultatuCommand());
-        keyboard.add(keyboardRow);
-        replyKeyboardMarkup.setKeyboard(keyboard);
+        String str[] = null;
         
+        switch(aukeraLista) {
+        case 0: str = hasiera;
+         break;
+        case 1: str = arrayGeltokiak;
+        	break;
+        case 2:str = arrayGeltokiak;
+    		break;
+        }
+  
+		for(String geltokia: str) {
+			KeyboardRow keyboardRow = new KeyboardRow();
+			keyboardRow.add(geltokia);
+			keyboard.add(keyboardRow);
+		}
+		
+		replyKeyboardMarkup.setKeyboard(keyboard);
 		return replyKeyboardMarkup;
 	}
-	
-    private static String getKontsultatuCommand() {
-        return "KONTSULTATU";
-    }
-
 }
