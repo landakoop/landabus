cc138b3516fee6806f0f1b3e9f460c875206e815
inigoarenaza <inigo.arenaza@alumni.mondragon.edu>
2019-05-27 12:40:52 +0200

    [#12] Lehen bertsioa

--- AdimenArtifiziala/v1.R
+++ AdimenArtifiziala/v1.R
@@ -35,3 +35,24 @@ lista<-unique(lista)
 
 result <- ga(type="permutation",  fitness=f, lower=1, upper=length(dis), popSize = 50, maxiter = 500,
              run = 500, pmutation = 0.2)
+
+datos <- fromJSON(getURL("http://localhost:8080/api/eskaera/list"))
+dis <- fromJSON(getURL("http://localhost:8080/api/geltokia/distantziak"))
+dataset<-data.frame(do.call("rbind",datos))
+
+num<-2
+indice<-c(2,6)
+datasetHorarios<-dataset[indice]
+kmeans.fit<-kmeans(datasetHorarios,num)
+list<-kmeans.fit$cluster
+dataset<-cbind(dataset,list)
+geltokiak<-NULL
+for(i in num){
+  cluster<-dataset[dataset$list==i,]
+  for(i in length(cluster)){
+    geltokiak<-c(geltokiak, cluster$irteera[i])
+    geltokiak<-c(geltokiak, cluster$helmuga[i])
+  }
+  geltokiak<-unique(c(cluster$irteera,cluster$helmuga))
+  geltokiak<-unique(geltokiak)
+}
