a86c7922019da77970efd320ac84be3aed9a7416
hodeiab03 <hodei.aguirre@alumni.mondragon.edu>
2019-06-04 15:31:11 +0200

    SOAPek behar dituen klaseak igo. POMa aldatu klaseak berriro ez sortzeko

--- main-app/src/main/java/landakoop/landabus/mainapp/soap/config/SoapConfig.java
+++ main-app/src/main/java/landakoop/landabus/mainapp/soap/config/SoapConfig.java
@@ -1,5 +1,7 @@
 package landakoop.landabus.mainapp.soap.config;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -23,6 +27,14 @@ public class SoapConfig extends WsConfigurerAdapter {
 		return new ServletRegistrationBean(servlet, "/ws/*");
 	}
 	
+ 	@Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        PayloadValidatingInterceptor validatingInterceptor = new PayloadValidatingInterceptor();
+        validatingInterceptor.setValidateRequest(true);
+        validatingInterceptor.setValidateResponse(true);
+        validatingInterceptor.setXsdSchema(autobusGeldialdiaSchema());
+        interceptors.add(validatingInterceptor);
+    }
 	
 	@Bean
 	public XsdSchema autobusGeldialdiaSchema() {
