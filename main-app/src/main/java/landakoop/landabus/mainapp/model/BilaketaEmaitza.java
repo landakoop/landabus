a18f5121a5a7633de3f17a4bcf6a712257bfaebb
inigoarenaza <inigo.arenaza@alumni.mondragon.edu>
2019-06-06 20:03:55 +0200

    [#21] Bilaketaren emaitza klasea

---
+++ main-app/src/main/java/landakoop/landabus/mainapp/model/BilaketaEmaitza.java
@@ -0,0 +1,30 @@
+package landakoop.landabus.mainapp.model;
+
+import java.util.List;
+
+public class BilaketaEmaitza {
+	List<Integer> linea;
+	List<Integer> eskaerak;
+	Integer irteeraOrdua;
+	
+	public List<Integer> getLinea() {
+		return linea;
+	}
+	public void setLinea(List<Integer> linea) {
+		this.linea = linea;
+	}
+	public List<Integer> getEskaerak() {
+		return eskaerak;
+	}
+	public void setEskaerak(List<Integer> eskaerak) {
+		this.eskaerak = eskaerak;
+	}
+	public Integer getIrteeraOrdua() {
+		return irteeraOrdua;
+	}
+	public void setIrteeraOrdua(Integer irteeraOrdua) {
+		this.irteeraOrdua = irteeraOrdua;
+	}
+	
+	
+}
