8d852f023398ae5f20eb3d3b17670f92e3a774cb
Bingen Galartza <bingen.galartza@alumni.mondragon.edu>
2019-06-11 10:04:27 +0200

    [#32] Telegram retry estrategia

--- LandabusBot/src/main/java/landakoop/landabus/landabusBot/GeltokiakJaso.java
+++ LandabusBot/src/main/java/landakoop/landabus/landabusBot/GeltokiakJaso.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import landakoop.landabus.beans.Geltokia;
 
 public class GeltokiakJaso {
+	private static final Logger logger = LoggerFactory.getLogger(GeltokiakJaso.class);
+	
 	@Value("${mainapp.protocol}"+"://"+"${mainapp.host}"+":"+"${mainapp.port}") 
 	String url;
 	
@@ -22,19 +26,31 @@ public class GeltokiakJaso {
 	}
 	
 	public List<Geltokia> geltokiakJaso() {
-		try {
-			lista = eskaeraEgin();
-		} catch (URISyntaxException e) {
-			//logger.error("Ezin izan da main-app etik geltoki zerrenda eskuratu. Excep= {} ", e.getClass());
+		boolean jaso = false;
+		while(!jaso) {
+			try {
+				lista = eskaeraEgin();
+				jaso = true;
+			} catch (Exception e) {
+				logger.error("Ezin izan da main-app etik geltoki zerrenda eskuratu. Excep= {} ", e.getClass());
+				itxaron(10000);
+			}
 		}
+
 		return lista;
 	}
 	
-	private static List<Geltokia>  eskaeraEgin() throws URISyntaxException {
+	public void itxaron(int milis) {
+		try {
+			Thread.sleep(milis);
+		} catch (InterruptedException e) {
+			logger.error("Itxarotea gelditu da");
+		}
+	}
+	
+	private List<Geltokia>  eskaeraEgin() throws URISyntaxException {
 		RestTemplate restTemplate = new RestTemplate();
-		URI uri = new URI("http://main-app:8080/api/geltokia/list");
-		//URI uri = new URI(url+"/api/geltokia/list");
-		//System.out.println(uri.toString());		
+		URI uri = new URI(url +"/api/geltokia/list");
 		return Arrays.asList(restTemplate.getForObject(uri, Geltokia[].class));
 	}
 
