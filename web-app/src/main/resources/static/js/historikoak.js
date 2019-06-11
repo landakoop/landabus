4556de0ed58119962917faac0fc028760faf0f38
Bingen Galartza <bingen.galartza@alumni.mondragon.edu>
2019-06-11 09:48:26 +0200

    [#32] Frontendean ruta erlatiboak erabili

--- web-app/src/main/resources/static/js/historikoak.js
+++ web-app/src/main/resources/static/js/historikoak.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON(':8080/api/ibilbidea/historikoa' + i,    function (data) {
+    $.getJSON('/api/ibilbidea/historikoa' + i,    function (data) {
         seriesOptions[i] = {
             name: name,
             data: data
