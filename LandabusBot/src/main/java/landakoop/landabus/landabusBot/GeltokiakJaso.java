ef5110f68aa701375523f56c854581d0c52c816f
Bingen Galartza <bingen.galartza@alumni.mondragon.edu>
2019-06-11 10:18:58 +0200

    [#32] Telegram env aldagaiak konpondu

--- LandabusBot/src/main/java/landakoop/landabus/landabusBot/GeltokiakJaso.java
+++ LandabusBot/src/main/java/landakoop/landabus/landabusBot/GeltokiakJaso.java
@@ -9,10 +9,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import landakoop.landabus.beans.Geltokia;
 
+@Component
 public class GeltokiakJaso {
 	private static final Logger logger = LoggerFactory.getLogger(GeltokiakJaso.class);
 	
